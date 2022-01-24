c85a0e4b43b168bd89f5cc4024824c2d6f9bebe8
omar chatin <omerchatin19@gmail.com>
2022-01-24 23:04:26 +0300

    some javascript functionalaty

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const rightnav = document.querySelector(".right-nav-pages");
 const main = document.querySelector(".main");
 const allplantscontainer = document.querySelector(".all-plants-container");
 
+
 menubtn.addEventListener("click", () => {
   menubtn.classList.toggle("open");
   document.querySelector(".line1").classList.toggle("open");
@@ -16,35 +17,36 @@ filtersbtn.addEventListener("click", () => {
   main.classList.toggle("filter-open");
 });
 
-// plantHtml
+const likedFunction=(liked)=>{
+  if(liked){
+    return `<img src="./img/heart-likes.png" alt="" />`;
+  }
+  else{
+    return `<img src="./img/notliked.png" alt="" />`;
+  }
+}
 
-/* <div class="plant">
-<div class="plant-like-icon">
-  <img src="./img/heart-likes.png" alt="" />
-</div>
-<div class="plant-img"></div>
-<div class="plant-top">
-  <div class="plant-name">Avokado tree</div>
-  <div class="plant-info-btn"></div>
-</div>
-<div class="plant-stars">
-  <div class="star fullstar"></div>
-  <div class="star fullstar"></div>
-  <div class="star fullstar"></div>
-  <div class="star emptystar"></div>
-  <div class="star emptystar"></div>
-  (85)
-</div>
-<div class="plant-bottom">
-  <div class="plant-price">$10</div>
-  <div class="plant-add-to-cart">add to cart</div>
-</div>
-</div> */
+
+const howmanystarsfunctionalaty=(stars)=>{
+  stars=Math.floor(stars/20);
+
+  let html='';
+  // full stars
+  for(let i=0;i<stars;i++){
+    html+=`<div class="star full-star"></div>`;
+  }
+  // empty stars
+  for(let i=0;i<5-stars;i++){
+    html+=`<div class="star empty-star"></div>`;
+  }
+  return html;
+}
+
 
 const plantHtml = (plantName, plantImg, plantPrice, plantStars, plantLikes, id, liked) => {
   return `<div class="plant" id=${id}>
-<div class="plant-like-icon">
-  <img src="./img/heart-likes.png" alt="" />
+<div class="plant-like-icon liked-${liked}">
+  ${likedFunction(liked)}
 </div>
 <div class="plant-img" style="background-image:url(${plantImg})"></div>
 <div class="plant-top">
@@ -52,12 +54,8 @@ const plantHtml = (plantName, plantImg, plantPrice, plantStars, plantLikes, id, 
   <div class="plant-info-btn"></div>
 </div>
 <div class="plant-stars">
-<div class="star fullstar"></div>
-<div class="star fullstar"></div>
-<div class="star fullstar"></div>
-<div class="star emptystar"></div>
-<div class="star emptystar"></div>
-(85)
+  ${howmanystarsfunctionalaty(plantStars)}
+(${plantStars})
 </div>
 <div class="plant-bottom">
   <div class="plant-price">$${plantPrice}</div>
@@ -75,7 +73,7 @@ const plabntsList=[
         name: "Avokado tree",
         img: "./img/plant-1.png",
         price: 10,
-        stars: 85,
+        stars: 90,
         likes: 100,
         id: 1,
         liked: false
@@ -84,16 +82,34 @@ const plabntsList=[
         name: "Avokado tree",
         img: "./img/plant-2.png",
         price: 10,
-        stars: 85,
+        stars: 11,
         likes: 100,
         id: 2,
-        liked: true
+        liked: false
     },
     {
         name: "Avokado tree",
         img: "./img/plant-1.png",
         price: 10,
-        stars: 85,
+        stars: 50,
+        likes: 100,
+        id: 3,
+        liked: false
+    },
+    {
+        name: "Avokado tree",
+        img: "./img/plant-1.png",
+        price: 10,
+        stars: 10,
+        likes: 100,
+        id: 3,
+        liked: false
+    },
+    {
+        name: "Avokado tree",
+        img: "./img/plant-1.png",
+        price: 10,
+        stars: 20,
         likes: 100,
         id: 3,
         liked: false
@@ -111,25 +127,7 @@ const plabntsList=[
         name: "Avokado tree",
         img: "./img/plant-1.png",
         price: 10,
-        stars: 85,
-        likes: 100,
-        id: 3,
-        liked: false
-    },
-    {
-        name: "Avokado tree",
-        img: "./img/plant-1.png",
-        price: 10,
-        stars: 85,
-        likes: 100,
-        id: 3,
-        liked: false
-    },
-    {
-        name: "Avokado tree",
-        img: "./img/plant-1.png",
-        price: 10,
-        stars: 85,
+        stars: 40,
         likes: 100,
         id: 3,
         liked: false
@@ -147,8 +145,8 @@ const plabntsList=[
         name: "Avokado tree",
         img: "./img/plant-2.png",
         price: 10,
-        stars: 85,
-        likes: 100,
+        stars: 100,
+        likes: 60,
         id: 3,
         liked: false
     },
@@ -164,3 +162,24 @@ for (let i=0;i<plabntsList.length;i++){
     const newPlant = plantHtml(plant.name, plant.img, plant.price, plant.stars, plant.likes, plant.id, plant.liked);
     allplantscontainer.innerHTML += newPlant;
 }
+
+
+
+
+
+
+
+
+const plantlikeicon=document.querySelectorAll(".plant-like-icon");
+plantlikeicon.forEach(icon=>{
+  icon.addEventListener("click",()=>{
+      icon.classList.toggle("liked");
+      icon.classList.toggle("liked-true");
+      if (icon.classList.contains("liked-true")){
+        icon.innerHTML=likedFunction(true);
+      }
+      else{
+        icon.innerHTML=likedFunction(false);
+      }
+  })
+})
